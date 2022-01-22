8fa23722e2299339efaa119c7789d6da414d25d7
loaki07 <loakesh.i@gmail.com>
2022-01-22 15:46:46 +0530

    add: basic_traits

--- mod_3/traits_in_rust/src/main.rs
+++ mod_3/traits_in_rust/src/main.rs
@@ -1,3 +1,18 @@
+mod basic_trait;
+
+use basic_trait::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("basic_trait in rust!");
+
+      let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
