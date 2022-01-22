9e914f0c8cf1e6ae6fbb7e689ed51e101b5b27ce
loaki07 <loakesh.i@gmail.com>
2022-01-22 14:35:39 +0530

    add: new_type_pattern.rs

--- mod_3/advanced_traits_in_rust/src/main.rs
+++ mod_3/advanced_traits_in_rust/src/main.rs
@@ -2,11 +2,13 @@ mod generic_type_paramenters;
 mod iterator_trait;
 mod methods_with_the_same_name;
 mod super_traits;
+mod new_type_pattern;
 
 use generic_type_paramenters::*;
 use iterator_trait::*;
 use methods_with_the_same_name::*;
 use super_traits::*;
+use new_type_pattern::*;
 
 fn main() {
     println!("advanced traits rust!");
@@ -37,4 +39,8 @@ fn main() {
  
     // super traits
     // SecondPoint{x: 1, y: 2}.outline_print();
+
+    // new_type_pattern
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
---
+++ mod_3/advanced_traits_in_rust/src/new_type_pattern.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+// chapter 10 of the rust-lang book 
+// orphan rule which states that we can 
+// implement a trait on a type as long as 
+// the trait of pattern is defined within our crate
+
+// the new type pattern allows us to get around 
+// this restriction by creatin a tupe struct with one
+// field which is going to be the type we are wrapping
+// this this wrapper is local to our crate
+// so we can implement a new trait for it
+pub struct Wrapper(pub Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
