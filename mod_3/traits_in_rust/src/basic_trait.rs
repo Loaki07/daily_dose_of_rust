5b4b3c40f094eaab1d203b1b740eb5673ca845f7
loaki07 <loakesh.i@gmail.com>
2022-01-26 13:31:50 +0530

    add: default implementations for trait functions

--- mod_3/traits_in_rust/src/basic_trait.rs
+++ mod_3/traits_in_rust/src/basic_trait.rs
@@ -1,5 +1,9 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
 pub struct NewsArticle {
@@ -10,9 +14,13 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
     }
+
+    // fn summarize(&self) -> String {
+    //     format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // }
 }
 
 pub struct Tweet {
@@ -23,6 +31,10 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
--- mod_3/traits_in_rust/src/main.rs
+++ mod_3/traits_in_rust/src/main.rs
@@ -5,14 +5,24 @@ use basic_trait::*;
 fn main() {
     println!("basic_trait in rust!");
 
-      let tweet = Tweet {
+    let tweet = Tweet {
         username: String::from("horse_ebooks"),
-        content: String::from(
-            "of course, as you probably already know, people",
-        ),
+        content: String::from("of course, as you probably already know, people"),
         reply: false,
         retweet: false,
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
