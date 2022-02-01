9bf6ed3a6caf982c5304e0aab94ab4f309469550
loaki07 <loakesh.i@gmail.com>
2022-02-01 10:37:21 +0530

    add: stroring words with its length

---
+++ mod_6/store_words_with_len/src/main.rs
@@ -0,0 +1,17 @@
+
+use std::collections::HashMap;
+
+fn main() {
+    println!("store words with len");
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
