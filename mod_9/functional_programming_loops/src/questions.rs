8ca5bdbc715ce3951b84368a0c7a72c779363213
loaki07 <loakesh.i@gmail.com>
2022-03-22 22:25:23 +0530

    add: solution to question_1

--- mod_9/functional_programming_loops/src/questions.rs
+++ mod_9/functional_programming_loops/src/questions.rs
@@ -1,16 +1,31 @@
+use core::num;
+use std::vec;
+
 pub fn question_1() {
     // given an array of numbers.
     // replace each number with the
     // product of all the numbers in the array
-    // except the number itself without using
-    // division
+    // except the number itself without division
+    // https://leetcode.com/problems/product-of-array-except-self/
+    // Example 1
+    // Input: nums = [1,2,3,4]
+    // Output: [24,12,8,6]
 
-    let mut numbers = vec![1, 2, 3, 4, 5];
+    let nums = vec![1, 2, 3, 4];
 
-    numbers
-        .iter_mut()
-        .enumerate()
-        .map(|(index, value)| *value * 2);
+    let mut result = vec![1; nums.len()];
 
-    numbers.iter().for_each(|x| println!("res: {}", x));
+    (0..nums.len()).fold(1i32, |mut sum, i| {
+        result[i] = sum;
+        sum *= nums[i];
+        sum
+    });
+
+    (0..nums.len()).rev().fold(1i32, |mut sum, i| {
+        result[i] *= sum;
+        sum *= nums[i];
+        sum
+    });
+
+    result.iter().for_each(|x| println!("res: {}", x));
 }
